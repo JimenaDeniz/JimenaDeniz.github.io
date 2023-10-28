29c1bdf01fbb8cc35c05195b30fb20ef4358cf11
Jimena Deniz Ruiz <147679079+JimenaDeniz@users.noreply.github.com>
2023-10-27 20:33:09 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #94ADD7;'>|</span>",
+ cursor: "<span style='color: #86709B;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #94ADD7;"> Soy estudiante y desarrolladora Frontend Jr <span>')
+  .typeString('<span style="color: #86709B;"> "El único viaje imposible es aquel que nunca comienzas" <span>')
+  
   .pauseFor(200)
   .deleteChars(10)
   .start();
